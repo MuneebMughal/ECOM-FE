154cd1c3e5255dce6f4e071c90ef4de68ac3dfa7
Muneeb <muneebmughalr@gmail.com>
2022-04-27 23:02:11 +0500

    Sidebar updated

--- src/Pages/coupons/Coupons.jsx
+++ src/Pages/coupons/Coupons.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
-
+import React from "react";
+import Layout from "../../layout/Layout";
 const Coupons = () => {
   return (
-    <div>Coupons</div>
-  )
-}
+    <Layout {...props}>
+      <div>Coupons</div>
+    </Layout>
+  );
+};
 
-export default Coupons
+export default Coupons;
